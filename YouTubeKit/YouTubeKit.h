7c343beb7fce143aa227a1caa4864324ab4d5a7a
matsuosh <shooohei.m@gmail.com>
2015-05-13 21:37:14 +0900

    Initial Commit

---
+++ YouTubeKit/YouTubeKit.h
@@ -0,0 +1,19 @@
+//
+//  YouTubeKit.h
+//  YouTubeKit
+//
+//  Created by matsuosh on 2015/05/13.
+//  Copyright (c) 2015年 matsuosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouTubeKit.
+FOUNDATION_EXPORT double YouTubeKitVersionNumber;
+
+//! Project version string for YouTubeKit.
+FOUNDATION_EXPORT const unsigned char YouTubeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouTubeKit/PublicHeader.h>
+
+
